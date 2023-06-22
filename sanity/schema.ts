e72e2ec5f7eb384428947f3007448a5c92144349
thelordzeus <arnav55125@gmail.com>
2023-06-23 04:46:31 +0530

    feat: added schemas for all the domains

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,28 @@
 import { type SchemaTypeDefinition } from "sanity";
 import corporate from "./schemas/corporate";
+import wad from "./schemas/wad";
+import comsoc from "./schemas/comsoc";
+import publicity from "./schemas/publicity";
+import rnd from "./schemas/rnd";
+import mnc from "./schemas/mnc";
+import ias from "./schemas/ias";
+import pels from "./schemas/pels";
+import wie from "./schemas/wie";
+import alumini from "./schemas/alumini";
+import sponsership from "./schemas/sponsership";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [corporate],
+  types: [
+    corporate,
+    wad,
+    comsoc,
+    publicity,
+    sponsership,
+    rnd,
+    mnc,
+    ias,
+    pels,
+    wie,
+    alumini,
+  ],
 };
---
+++ sanity/schemas/comsoc.ts
@@ -0,0 +1,46 @@
+const comsoc = {
+  name: "comsoc",
+  title: "Computer Society",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Enter the Name",
+      type: "string",
+    },
+    {
+      name: "designation",
+      title: "Enter the Designation",
+      type: "string",
+    },
+    {
+      name: "gitUrl",
+      title: "Enter the Github URL",
+      type: "url",
+    },
+    {
+      name: "linkedInUrl",
+      title: "Enter the LinkedIn URL",
+      type: "url",
+    },
+    {
+      name: "portfolioUrl",
+      title: "Enter the Portfolio URL",
+      type: "url",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      Options: { hotspot: true },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+        },
+      ],
+    },
+  ],
+};
+
+export default comsoc;

40b7d02001f92b84dcaaf53e946714cad59d662e
thelordzeus <arnav55125@gmail.com>
2023-06-23 04:50:31 +0530

    feat: added types declaration

---
+++ types/member.ts
@@ -0,0 +1,11 @@
+
+export type member = {
+    _id: string,
+    _createdAt: Date,
+    name: string,
+    designation: string,
+    gitUrl: string,
+    linkedInUrl: string,
+    portfolioUrl: string,
+    image: string,
+}

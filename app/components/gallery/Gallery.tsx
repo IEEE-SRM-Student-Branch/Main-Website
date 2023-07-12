0bf93712abb6b1995875aca7f3ea35fad40e9f0e
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-07-12 19:33:08 +0530

    Made component responsive for mobile

--- app/components/gallery/Gallery.tsx
+++ app/components/gallery/Gallery.tsx
@@ -32,12 +32,12 @@ const Gallery = () => {
 
 
     return (
-        <div className = "bg-primarybg text-white w-full h-[900px] flex flex-row">
-            <div id = "galleryHeader" className = "flex flex-col align-middle lg:mx-24 mx-12 w-1/3">
-                <h1 className = "font-sans font-bold lg:text-6xl/[128px] sm:text-4xl/[96px]">
+        <div className="bg-primarybg text-white w-full h-[1000px] flex flex-row max-lg:flex-col">
+            <div id="galleryHeader" className="flex flex-col align-middle my-5 lg:mx-24 mx-12 w-1/3 max-lg:w-4/5">
+                <h1 className="font-sans font-bold my-3 max-lg:my-5 text-4xl lg:text-5xl/[60px] sm:text-4xl">
                     Our Gallery
                 </h1>
-                <p className = "font-sans lg:text-xl/[32px] sm:text-xs/[18px] font-base text-justify w-full">
+                <p className="font-sans lg:text-lg my-5 font-base text-justify w-full">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum et ante ac fringilla.
                     Pellentesque et felis metus. Cras facilisis ullamcorper magna et elementum. Vestibulum aliquet mi
                     quam, non imperdiet erat laoreet sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -51,70 +51,117 @@ const Gallery = () => {
 
             {/*Left Side Moving Carousel*/}
 
-            <div id = "carousel-container" className = "relative grid place-items-center overflow-hidden before:top-0 after:bottom-0">
-                <div id = "carousel" className = {`flex flex-col animate-infiniteScroll h-[calc(360px * ${leftLength*2})]`} style={{ '--leftLength': leftLength }}>
+            <div id="carousel-container"
+                 className="relative grid place-items-center overflow-hidden before:top-0 after:bottom-0 max-lg:my-1"
+            >
+                <div id="carousel"
+                     className={`flex lg:flex-col max-lg:items-center lg:animate-infiniteScroll max-lg:animate-infiniteScrollMobile lg:h-[calc(360px * ${leftLength * 2})] max-lg:h-[300px] max-lg:w-[calc(360px * ${leftLength * 2})] max-ml:h-[200px] mx-5`}
+                     style={{'--leftLength': leftLength}}
+                >
                     {imagesLeft.map((image) => (
-                        <div id = "slide" className="flex justify-center my-[20px]">
+                        <div id="slide" className="flex justify-center lg:my-[20px] max-lg:mx-[20px] max-lg:w-[300px] max-lg:h-[300px] max-ml:w-[200px] max-ml:h-[200px]">
                             <Image
-                                key = {image.id}
-                                src = {image.src}
-                                alt = {image.alt}
-                                width = {360}
-                                height = {240}
-                                id= "image"
-                                className = "h-auto w-auto"
+                                key={image.id}
+                                src={image.src}
+                                alt={image.alt}
+                                width={360}
+                                height={240}
+                                id="image"
+                                className="lg:h-auto lg:w-auto"
                             />
                         </div>
                     ))}
 
                     {imagesLeft.map((image) => (
-                        <div id = "slide" className="flex justify-center my-[20px]">
+                        <div id="slide" className="flex justify-center lg:my-[20px] max-lg:mx-[20px] max-lg:w-[300px] max-lg:h-[300px] max-ml:w-[200px] max-ml:h-[200px]">
                             <Image
-                                key = {image.id}
-                                src = {image.src}
-                                alt = {image.alt}
-                                width = {360}
-                                height = {240}
-                                id= "image"
-                                className = "h-auto w-auto"
+                                key={image.id}
+                                src={image.src}
+                                alt={image.alt}
+                                width={360}
+                                height={240}
+                                id="image"
+                                className="lg:h-auto lg:w-auto"
                             />
                         </div>
                     ))}
+
                 </div>
             </div>
 
 
             {/*Right Side Moving Carousel*/}
 
-            <div id = "carousel-container" className = "relative grid place-items-center overflow-hidden before:top-0 after:bottom-0">
-                <div id = "carousel" className = {`flex flex-col animate-infiniteScrollReverse h-[calc(360px * ${rightLength*2})]`} style={{ '--rightLength': rightLength }}>
+            {/*<div id="carousel-container"*/}
+            {/*     className="relative grid place-items-center overflow-hidden lg:before:top-0 lg:after:bottom-0">*/}
+            {/*    <div id="carousel"*/}
+            {/*         className={`flex lg:flex-col animate-infiniteScrollReverse max-lg:animate-infiniteScrollReverseMobile h-[calc(360px * ${rightLength * 2})] max-lg:w-[calc(360px * ${rightLength * 2})]`}*/}
+            {/*         style={{'--rightLength': rightLength}}*/}
+            {/*    >*/}
+            {/*        {imagesRight.map((image) => (*/}
+            {/*            <div id="slide" className="flex justify-center my-[20px] mx-10">*/}
+            {/*                <Image*/}
+            {/*                    key={image.id}*/}
+            {/*                    src={image.src}*/}
+            {/*                    alt={image.alt}*/}
+            {/*                    width={360}*/}
+            {/*                    height={240}*/}
+            {/*                    id="image"*/}
+            {/*                    className="h-auto w-auto"*/}
+            {/*                />*/}
+            {/*            </div>*/}
+            {/*        ))}*/}
+
+            {/*        {imagesRight.map((image) => (*/}
+            {/*            <div id="slide" className="flex justify-center my-[20px] mx-10">*/}
+            {/*                <Image*/}
+            {/*                    key={image.id}*/}
+            {/*                    src={image.src}*/}
+            {/*                    alt={image.alt}*/}
+            {/*                    width={360}*/}
+            {/*                    height={240}*/}
+            {/*                    id="image"*/}
+            {/*                    className="h-auto w-auto"*/}
+            {/*                />*/}
+            {/*            </div>*/}
+            {/*        ))}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+
+            <div id="carousel-container"
+                 className="relative grid place-items-center overflow-hidden before:top-0 after:bottom-0 max-lg:my-10">
+                <div id="carousel"
+                     className={`flex lg:flex-col max-lg:items-center lg:animate-infiniteScrollReverse max-lg:animate-infiniteScrollReverseMobile lg:h-[calc(360px * ${rightLength * 2})] max-lg:h-[300px] max-lg:w-[calc(360px * ${rightLength * 2})] max-ml:h-[200px] mx-5`}
+                     style={{'--rightLength': rightLength}}
+                >
                     {imagesRight.map((image) => (
-                        <div id = "slide" className="flex justify-center my-[20px] mx-10">
+                        <div id="slide" className="flex justify-center lg:my-[20px] max-lg:mx-[20px] max-lg:w-[300px] max-lg:h-[300px] max-ml:w-[200px] max-ml:h-[200px]">
                             <Image
-                                key = {image.id}
-                                src = {image.src}
-                                alt = {image.alt}
-                                width = {360}
-                                height = {240}
-                                id= "image"
-                                className = "h-auto w-auto"
+                                key={image.id}
+                                src={image.src}
+                                alt={image.alt}
+                                width={360}
+                                height={240}
+                                id="image"
+                                className="lg:h-auto lg:w-auto"
                             />
                         </div>
                     ))}
 
                     {imagesRight.map((image) => (
-                        <div id = "slide" className="flex justify-center my-[20px] mx-10">
+                        <div id="slide" className="flex justify-center lg:my-[20px] max-lg:mx-[20px] max-lg:w-[300px] max-lg:h-[300px] max-ml:w-[200px] max-ml:h-[200px]">
                             <Image
-                                key = {image.id}
-                                src = {image.src}
-                                alt = {image.alt}
-                                width = {360}
-                                height = {240}
-                                id= "image"
-                                className = "h-auto w-auto"
+                                key={image.id}
+                                src={image.src}
+                                alt={image.alt}
+                                width={360}
+                                height={240}
+                                id="image"
+                                className="lg:h-auto lg:w-auto"
                             />
                         </div>
                     ))}
+
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,18 +18,31 @@ module.exports = {
             colors: {
                 "primarybg": "#030214",
             },
+            screens: {
+                "ml": "425px" //Stands for Mobile Large
+            },
             animation : {
-                "infiniteScroll": "scroll 30s linear infinite",
-                "infiniteScrollReverse": "scrollReverse 30s linear infinite",
+                "infiniteScroll": "scroll 40s linear infinite",
+                "infiniteScrollReverse": "scrollReverse 40s linear infinite",
+                "infiniteScrollMobile": "scrollMobile 40s linear infinite",
+                "infiniteScrollReverseMobile": "scrollReverseMobile 40s linear infinite",
             },
             keyframes : {
                 scroll : {
                     "0%" : { transform : "translateY(0)" },
-                    "100%" : { transform : "translateY(calc(-360px * var(--leftLength)))" },
+                    "100%" : { transform : "translateY(calc(-400px * var(--leftLength)))" },
+                },
+                scrollMobile : {
+                    "0%" : { transform : "translateX(0)" },
+                    "100%" : { transform : "translateX(calc(-400px * var(--leftLength)))" },
                 },
                 scrollReverse : {
                     "100%" : { transform : "translateY(0)" },
-                    "0%" : { transform : "translateY(calc(-360px * var(--rightLength)))" },
+                    "0%" : { transform : "translateY(calc(-400px * var(--rightLength)))" },
+                },
+                scrollReverseMobile : {
+                    "100%" : { transform : "translateX(0)" },
+                    "0%" : { transform : "translateX(calc(-400px * var(--rightLength)))" },
                 }
             },
         },
